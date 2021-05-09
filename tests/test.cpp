af2b228e8db0797d88bf3d3df470adb717f04a9f
supsun-sockol <supsun-sockol@yandex.ru>
2021-05-09 19:41:19 +0300

    acc

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,10 +3,37 @@
 
 #include "gtest/gtest.h"
 
-TEST(Print, TestSuperTest)
+TEST(Account, test1)
 {
-	EXPECT_EQ(3, 5);
+	Account A(1, 5);
+	EXPECT_EQ(A.GetBalance(), 5);
 }
-int main (){
+TEST(Account, test2)
+{
+	Account A(1, 5);
+	A.ChangeBalance(3);
+	EXPECT_EQ(A.GetBalance(), 3);
+}
+TEST(Account, test3)
+{
+	Account A(1, 5);
+	A.Lock();
+	EXPECT_ANY_THROW(A.ChangeBalance(3));
+}
+TEST(Account, test4)
+{
+	Account A(1, 5);
+	A.Lock();
+	EXPECT_ANY_THROW(A.Lock());
+}
+
+TEST(Account, test5)
+{
+	Account A(1, 5);
+	A.Lock();
+	A.Unlock();
+	EXPECT_NO_THROW(A.ChangeBalance(3));
+}
+int main(){
 	return RUN_ALL_TESTS();
 }
