3de9e8d105ad69a099180c5cbb699ec9f54406b2
supsun-sockol <supsun-sockol@yandex.ru>
2021-05-09 19:53:43 +0300

    acc3

--- tests/test.cpp
+++ tests/test.cpp
@@ -11,7 +11,7 @@ TEST(Account, test1)
 TEST(Account, test2)
 {
 	Account A(1, 5);
-	A.Unlock();
+	A.Lock();
 	A.ChangeBalance(3);
 	EXPECT_EQ(A.GetBalance(), 3);
 }
@@ -23,14 +23,14 @@ TEST(Account, test3)
 TEST(Account, test4)
 {
 	Account A(1, 5);
+	A.Lock();
 	EXPECT_ANY_THROW(A.Lock());
 }
 
 TEST(Account, test5)
 {
 	Account A(1, 5);
-	A.Unlock();
-	EXPECT_NO_THROW(A.ChangeBalance(3));
+	EXPECT_ANY_THROW(A.ChangeBalance(3));
 }
 int main(){
 	return RUN_ALL_TESTS();
