8e1714d1264300c027694e51f3ffaae305667e5f
supsun-sockol <supsun-sockol@yandex.ru>
2021-05-09 15:22:11 +0300

    add gtest

--- banking/tests/test.cpp
+++ banking/tests/test.cpp
@@ -1,13 +1,11 @@
-#include <Account.h>
+#include <../Account.h>
 //#include <Transaction.h>
 
 #include <gtest/gtest.h>
 
-int main(){
-	TEST(Print, TestSuperTest)
-	{
-		Account A(3, 2);
+TEST(Print, TestSuperTest)
+{
+	Account A(3, 2);
+	EXPECT_EQ(A.GetBalance(), 2);
+}
 
-	  EXPECT_EQ(A.GetBalance(), 2);
-	}
-};
